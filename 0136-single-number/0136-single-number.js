02a059230dfdf5ddaef7586ee74f7e465de7819c
Faheem-Shan <145975947+Faheem-Shan@users.noreply.github.com>
2025-09-22 14:11:53 +0530

    Time: 572 ms (5.03%), Space: 56.4 MB (60.03%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let arr=nums.filter((num,_,nums)=>nums.indexOf(num)==nums.lastIndexOf(num));
+    return +arr.join("")
+
+
+};
